3b3eb1ab6a8c4bc647f85f80b8ea3f24f2fe24e9
U-Mark-THINK\Mark <Mark@Mark-THINK.(none)>
2014-09-13 02:18:41 -0400

    successfully highlighting text

---
+++ content.js
@@ -0,0 +1,16 @@
+﻿function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type == "Text") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+$('body').mouseup(function (e) {
+    var text = getSelectionText();
+    if (text) {
+        alert(getSelectionText());
+    }
+});
